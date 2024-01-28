79740341eb9852a209114b1d3843341b06e8e3c6
Somin Lee <81960250+purin48@users.noreply.github.com>
2024-01-28 15:15:16 +0900

    setting : CORS 테스트 코드 추가 (#12)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  // CORS test
+  // CORS: 리소스 접근 허용
+  axios.defaults.headers["Access-Control-Allow-Origin"] = "*";
+
+  // CORS: 서로 다른 도메인간 쿠키 전달 허용
+  axios.defaults.withCredentials = true;
+  const test = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080"); // 백엔드 URL을 여기에 넣어주세요
+      console.log("API 호출 결과:", response.data);
+    } catch (error) {
+      console.error("API 호출 중 오류 발생:", error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +33,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={test}>test button</button>
       </header>
     </div>
   );
