dec14df128699c9ec3b143267aad5be38e42bb71
alsth712 <alsth712@naver.com>
2024-01-29 12:49:34 +0900

    chore : 카테고리 컴포넌트 추가

---
+++ frontend/src/components/Category.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Category() {
+  return (
+    <>
+      <h1>Category</h1>
+      <ul className="categoryList">
+        <li>A</li>
+        <li>B</li>
+        <li>C</li>
+        <li>D</li>
+      </ul>
+    </>
+  );
+}
