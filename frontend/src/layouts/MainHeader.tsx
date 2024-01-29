7dcb2cda5abe8e6565a85be7219016eae97458ff
alsth712 <alsth712@naver.com>
2024-01-29 12:46:26 +0900

    chore : 라우터 추가

---
+++ frontend/src/layouts/MainHeader.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function MainHeader() {
+  return (
+    <>
+      <h1>Header 입니다.</h1>
+      <ul className="headerList">
+        <li>
+          <Link to="/">메인</Link>
+        </li>
+        <li>
+          <Link to="/search">검색 화면</Link>
+        </li>
+        <li>
+          <Link to="/result">검색결과 화면</Link>
+        </li>
+      </ul>
+    </>
+  );
+}
---
+++ frontend/src/pages/Result.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Result() {
+  return <div>결과화면</div>;
+}
