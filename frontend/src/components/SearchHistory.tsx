16ac32024dab413dd4d635ec1cf174dbcc8628d6
alsth712 <alsth712@naver.com>
2024-01-30 18:03:24 +0900

    feat : 검색 결과 및 검색 내역 컴포넌트 추가

---
+++ frontend/src/components/SearchHistory.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function SearchHistory() {
+  return (
+    <>
+      <div>
+        <h3>검색 내역</h3>
+        {/* TODO: 검색 내역 리스트 조회/삭제 추가 */}
+      </div>
+    </>
+  );
+}
