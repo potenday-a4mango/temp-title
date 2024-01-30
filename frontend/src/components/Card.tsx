cbddc8ca3b8ec335dc70660aad1229c343ccfd91
alsth712 <alsth712@naver.com>
2024-01-30 18:03:39 +0900

    chore : 기존 card 컴포넌트 수정

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -17,35 +17,29 @@ export default function Card({
 
   return (
     <>
-      <div>
+      {/* TODO: style 차후 수정 / loading 화면 넣기 */}
+      <a href={cardItem.workUrl} target="_blank" rel="noopener noreferrer">
         <div>
-          {/* TODO: style 차후 수정 / loading 화면 넣기 */}
-          <a href={cardItem.workUrl}>
+          <div>
             <img
               src={cardItem.imageUrl}
               onContextMenu={preventImgClick}
               onDragStart={preventImgClick}
               width="250px"
             />
-          </a>
+          </div>
+          <div>
+            <h5>{cardItem.authorName}</h5>
+            <h5>{`${cardItem.count}회`}</h5>
+          </div>
+          <h5>{`@${cardItem.instargramId}`}</h5>
+          <ul>
+            {cardItem.workSubjectList.map((item: HashTagItem, idx: number) => (
+              <HashTag key={idx} hashTagItem={item} />
+            ))}
+          </ul>
         </div>
-        <h5>작품명 {cardItem.workName}</h5>
-        <h5>작가명 {cardItem.authorName}</h5>
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href={`https://www.instagram.com/${cardItem.instargramId}`}
-        >
-          <h5>작가계정 {`@${cardItem.instargramId}`}</h5>
-        </a>
-        <ul>
-          해시태그
-          {cardItem.workSubjectList.map((item: HashTagItem, idx: number) => (
-            <HashTag key={idx} hashTagItem={item} />
-          ))}
-        </ul>
-        <h5>조회수 {`${cardItem.count}회`}</h5>
-      </div>
+      </a>
       {/* TODO: style 차후 수정 */}
       <br />
       <hr />
