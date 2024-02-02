be15a25b2830f439ccf46ca79a5fd9a08d16b4fb
alsth712 <alsth712@naver.com>
2024-02-02 18:46:50 +0900

    style : 전체 레이아웃 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import App from './App';
+import './style/global.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <BrowserRouter>
     <RecoilRoot>
-      <App />
+      <div className="bg-black">
+        <App />
+      </div>
     </RecoilRoot>
   </BrowserRouter>,
 );
