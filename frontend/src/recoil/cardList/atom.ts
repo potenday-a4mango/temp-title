46975f6d031402810616c28d971331e02296f5c8
alsth712 <alsth712@naver.com>
2024-02-02 18:46:03 +0900

    chore : cardList 기본값 변경

--- frontend/src/recoil/cardList/atom.ts
+++ frontend/src/recoil/cardList/atom.ts
@@ -4,7 +4,7 @@ import type { WorkCategoryList } from './types';
 // category 상태 관리
 const workCategoryListState = atom<WorkCategoryList>({
   key: 'workCategoryListState',
-  default: '',
+  default: '전체',
 });
 
 export default workCategoryListState;
